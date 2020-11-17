144ef7d71d64d9064749ddb0ad814312fbca4f32
Dane Summers <dsummersl@yahoo.com>
2020-11-17 10:23:40 -0500

    Convert nonstandard CSV to standard CSV.

--- openfido.sh
+++ openfido.sh
@@ -1,7 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
+# nounset: undefined variable outputs error message, and forces an exit
+set -u
 # errexit: abort script at first error
 set -e
+# print command to stdout before executing it:
+set -x
 
 echo "OPENFIDO_INPUT = $OPENFIDO_INPUT"
 echo "OPENFIDO_OUTPUT = $OPENFIDO_OUTPUT"
@@ -48,6 +52,8 @@ python3 -u run_gridlabd_main.py \
 
 mv tmp/* $OPENFIDO_OUTPUT
 mv ESCs.glm $OPENFIDO_OUTPUT
-mv *.csv $OPENFIDO_OUTPUT
+for i in *.csv; do
+  cat $i | awk '/^# / { lastpound=$0; gsub(/^# /,"",lastpound) } !/^# / { if(lastpound) print lastpound ; print; lastpound="" }' | sed 's/^property.. //' > $OPENFIDO_OUTPUT/$i
+done
 mv gridlabd.* $OPENFIDO_OUTPUT
 mv *.txt $OPENFIDO_OUTPUT
